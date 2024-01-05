4623ee9f4eaf85332daf1358dd2c894436bce62d
Tit Kovalenko <t.a.kvlnk@gmail.com>
2024-01-05 21:18:25 +0200

    feat: initial commit with code to update one cell

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { google } from 'googleapis';
+import { authenticate } from '@google-cloud/local-auth';
+
+const auth = await authenticate({
+  keyfilePath: '/home/tito/projects/mono-to-gsheet/client-secret.json', // Path to your OAuth credentials file
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+// const auth = 'AIzaSyCxXpmfiiTYHlCPaHTg2WB0g4LbKFD4f-Y';
+
+const sheetId = '1TcpjYSMU6_AegrKyCfYJvcuTcWdzEKT1dHpgScGEo2Q';
+const range = 'Dec2023!A1'; // Update the sheet and cell as needed
+
+const sheets = google.sheets({ version: 'v4', auth });
+
+// Update the cell with new data
+sheets.spreadsheets.values.update({
+  spreadsheetId: sheetId,
+  range: range,
+  valueInputOption: 'RAW',
+  requestBody: {
+    values: [['EVEN MORE New Data!!!!!!111234455']],
+  },
+}, (err, res) => {
+  if (err) return console.error('The API returned an error:', err.message);
+  console.log('Cell updated successfully!');
+});
